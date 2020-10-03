f10509f7ee9c2022779a17cebeff31fafc636e63
rin <icedshytea@outlook.com>
2020-10-03 17:16:50 +0800

    Change date & time in editor to chips

--- app/src/main/java/io/github/icedshytea/journal/utils/datetime/DateTimeHelper.kt
+++ app/src/main/java/io/github/icedshytea/journal/utils/datetime/DateTimeHelper.kt
@@ -13,4 +13,20 @@ object DateTimeHelper {
 
         return dateTime.format(dateTimeFormatter)
     }
+
+    @JvmStatic
+    fun formatDate(dateTime: LocalDateTime): String {
+        val dateTimeFormatString = DateFormat.getBestDateTimePattern(Locale.getDefault(), "dd MMM yyyy")
+        val dateTimeFormatter = DateTimeFormatter.ofPattern(dateTimeFormatString)
+
+        return dateTime.format(dateTimeFormatter)
+    }
+
+    @JvmStatic
+    fun formatTime(dateTime: LocalDateTime): String {
+        val dateTimeFormatString = DateFormat.getBestDateTimePattern(Locale.getDefault(), "HH:mm")
+        val dateTimeFormatter = DateTimeFormatter.ofPattern(dateTimeFormatString)
+
+        return dateTime.format(dateTimeFormatter)
+    }
 }
